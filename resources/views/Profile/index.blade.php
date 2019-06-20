93ecc14520f6fe38f70b3c0a6fd5d9172d838e62
unknown <surajmaurya450@gmail.com>
2019-06-20 16:41:28 +0530

    Role Based Authentication is created with bugs

---
+++ resources/views/Profile/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.landing')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1>
+                Profile view here
+            </h1>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/about','AboutController@index')->name('about');
 Route::get('/contact','ContactController@index')->name('contact');
 Route::get('/supporter','SupporterController@index')->name('supporter');
+
+Route::get('/profile','ProfileController@index')->name('profile');
+
+Route::get('/admin','AdminController@index');
+Route::get('/superadmin','SuperAdminController@index');
